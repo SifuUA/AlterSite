b7871459d58148765ccc925c3be42402ec955bfb
SifuUA <alekseykres@gmail.com>
2017-12-13 17:52:40 +0200

    Initial commit

---
+++ src/main/java/com/okres/app/altersite/AltersiteApplication.java
@@ -0,0 +1,12 @@
+package com.okres.app.altersite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltersiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltersiteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/okres/app/altersite/AltersiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.okres.app.altersite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AltersiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
